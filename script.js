4823834ac6905cdeae19a32fe40ecfa4e2605065
nitin8857888 <nmungilwar@webuy.com>
2023-05-12 13:38:31 +0530

    add,sub,mul,div completed

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 let string='';
 let numbers=document.querySelectorAll('.number');
+let operators=document.querySelectorAll('.operator');
+let clearButton = document.querySelector('.clear');
+let outputResult=document.querySelector('input');
+let elements=[...numbers,...operators];
 
-Array.from(numbers).forEach((number)=>{
-    number.addEventListener('click',(e)=>{
-        if(e.target.innerHtml=='='){
-            string=eval(string);
-            document.querySelector('.row1').value=string;
-        }else if(e.target.innerHTML == 'C'){
-            string = ""
-            document.querySelector('.row1').value = string;
-          } else{ 
-            
-            string = string + e.target.innerHTML;
-            document.querySelector('.row1').value = string;
-              }
-    })
+elements.forEach((element)=>{
+  element.addEventListener('click',(e)=>{
+    
+
+    if(e.target.dataset.value=="="){
+      string=eval(string);
+      console.log(string);
+      document.querySelector('.row1').value=string;
+
+    }else{
+      
+      string=string+e.target.dataset.value;
+      outputResult.value=string;
+
+    }
+  
+
+  })
+  
 })
