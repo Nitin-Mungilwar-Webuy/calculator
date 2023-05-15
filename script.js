6bc77ca5afc432ed23d1273ce87bbfe7117e8346
nitin8857888 <nmungilwar@webuy.com>
2023-05-15 11:29:39 +0530

    optimised with switch case

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 let string='';
-let numbers=document.querySelectorAll('.number');
-let operators=document.querySelectorAll('.operator');
-let clearButton = document.querySelector('.clear');
-let outputResult=document.querySelector('input');
-let elements=[...numbers,...operators];
+const numbers=document.querySelectorAll('.number');
+const operators=document.querySelectorAll('.operator');
+const clearButton = document.querySelector('.clear');
+const outputResult=document.querySelector('input');
+const elements=[...numbers,...operators];
+
 
 elements.forEach((element)=>{
   element.addEventListener('click',(e)=>{
     
+    switch (e.target.dataset.value) {
+      case "=":
+        string=eval(string);
+        console.log(string);
+        outputResult.value=string;
+        break;
 
-    if(e.target.dataset.value=="="){
-      string=eval(string);
-      console.log(string);
-      document.querySelector('.row1').value=string;
+      case "ClearOneChar":
+        string=string.toString().slice(0,-1);
+        outputResult.value=string;
+        break;
 
-    }else if(e.target.dataset.value=="ClearOneChar"){
-      string=string.toString().slice(0,-1);
-      outputResult.value=string;
+      case "ClearAll":
+        string="";
+        outputResult.value=string;
+        break;
 
-    }else if(e.target.dataset.value=="ClearAll"){
-      string="";
-      outputResult.value=string;
-
+      default:
+        string=string+e.target.dataset.value;
+        outputResult.value=string;
+        break;
     }
-    else{
-      
-      string=string+e.target.dataset.value;
-      outputResult.value=string;
-
-    }
-  
 
   })
-  
 })
