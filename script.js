5e2d841777ee657873eefd6629b2f484defb0c8d
Nitin <132347798+nitin8857888@users.noreply.github.com>
2023-05-15 19:24:40 +0530

    changes completed

--- script.js
+++ script.js
@@ -8,51 +8,13 @@ elementsArray.forEach((element) => {
   element.addEventListener("click", (e) => {
     switch (e.target.dataset.value) {
       case "=":
-        string = eval(string); //test
+        string = eval(string); //test 123456
         outputResult.value = string;
         break;
       case "ClearOneChar":
         string = string.toString().slice(0, -1);
         outputResult.value = string;
         break;
-      case "ClearAll":
-        string = "";
-        outputResult.value = string;
-        break;
-      case "percentage":
-        string = eval(string) / 100;
-        outputResult.value = string;
-        break;
-
-      default:
-        string = string + e.target.dataset.value;
-        outputResult.value = string;
-        break;
-    }
-  });
-});
-
-let string = "";
-const numbersKeys = document.querySelectorAll(".numbersKeys");
-const operatorsKeys = document.querySelectorAll(".operatorsKeys");
-const clearButtonKey = document.querySelector(".clear");
-let outputResult = document.querySelector("input");
-const elementsArray = [...numbersKeys, ...operatorsKeys];
-
-elementsArray.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    switch (e.target.dataset.value) {
-      case "=":
-        string = eval(string);
-
-        outputResult.value = string;
-        break;
-
-      case "ClearOneChar":
-        string = string.toString().slice(0, -1);
-        outputResult.value = string;
-        break;
-
       case "ClearAll":
         string = "";
         outputResult.value = string;
